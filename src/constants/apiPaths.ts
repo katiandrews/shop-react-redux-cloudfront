deb72e785ff19dbe99e0433f16ce7714164aeb18
Ekaterina Andriushina <ekaterina_andriushina@epam.com>
2022-08-23 12:53:03 +0300

    use cart-api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://mp3k14edb2.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://inf7uhotw1.execute-api.eu-west-1.amazonaws.com',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://katiandrews-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
